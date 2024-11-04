936a23dbacb53679584c97638d5a85ce844819f0
imyemna <imadymna75@gmail.com>
2024-11-04 21:36:07 +0100

    ii

---
+++ ex2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    float b;
+    char c;
+    printf("enter un nomber et un réel et un caracter \n");
+    scanf("%d %f %c",&a ,&b,&c);
+    printf("le nombre est %d \n le réel est %f \n le caracter est %c",a,b,c);
+}
