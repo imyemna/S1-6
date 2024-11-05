fca8183fb41786af09c3a145f018372dea172083
imyemna <imadymna75@gmail.com>
2024-11-05 08:21:08 +0100

    oo

---
+++ programation c s1/tp1/ex3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int in1,in2;
+    printf("Enter premier number: ");
+    scanf("%d",&in1);
+    printf("Enter deuxieme number: ");
+    scanf("%d",&in2);
+    printf("le produit de %d et %d est :%d",in1,in2,in1*in2);
+}
