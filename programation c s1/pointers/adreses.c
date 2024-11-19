a39060281ca9a6419dfcc36cd4806b33a7e11515
imyemna <yemna.imad.24@ump.ma>
2024-11-19 14:03:54 +0100

    ff

--- programation c s1/pointers/adreses.c
+++ programation c s1/pointers/adreses.c
@@ -4,10 +4,16 @@ int main()
 {
     int a = 10;
     int *p = &a;
+    int **k=&p;
     printf("%i\n",a); // done la valeur
     printf("%p\n",a); // covertie la valeur en hexa
     printf("%p\n",&a); // done la l'adresse du variable a
-    printf("%i\n",p); //
+    printf("%i\n",p); // done la valeur de la variable p
     printf("%i\n",*p); //done la valeur de la variable a
     printf("%p\n",&p); //done la l'adresse de la variable p
+    printf("%i\n",*k); //done la valeur de la variable p
+    printf("%p\n",&k); //done la l'adresse de la variable k
 }
+// %p est pour l'adresse
+// * est pour extracter la valeur 
+// & est pour obtenir l'adresse
