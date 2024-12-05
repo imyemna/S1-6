d151830e095b6c43d15ee1f6e52b1ee2404450b7
imyemna <imadymna75@gmail.com>
2024-12-05 14:55:51 +0100

    ff

---
+++ programation c s1/allOthers/imagemr.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main ()
+{
+    int a,b,c;
+    scanf("%d",&a);
+    printf("%d",a);
+
+    while (a != 0)
+    {
+        b=a%10;
+        
+        c= c * 10 + b;
+
+        a /= 10;
+    }
+    
+    printf("image miroire de a est %d",c);
+
+}
